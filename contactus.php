eb86b9f64daa4b8f002c2673714abf7e734eba93
Ling Yang <yangling@perhapsfeel-lm.router>
2014-05-03 22:23:35 +0800

    add message board

--- contactus.php
+++ contactus.php
@@ -1,3 +1,20 @@
+<?php
+
+    //$to = "yangling_1985@126.com";
+    $to = "yangling1985@gmail.com";
+    $subject = $_POST["title"];
+    $message = $_POST["desc"];
+    $header = "From:{$_POST["name"]}<{$_POST['mail']}> \r\n";
+    if ($subject && $message) {
+        $retval = mail($to,$subject,$message,$header);
+        if(!$retval) 
+        {
+             error_log("Mail sent failed");
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -45,7 +62,21 @@
         <a href="contactus.php">联系我们</a>
     </div>
 
-<div class="section active" id="contactus_0"></div>
+<div class="section active" id="contactus_0">
+    <div id="email">
+        <form action="contactus.php" method="post">
+            * <input type="text" name="name" placeholder="名字"/><br>
+            * <input type="text" name="mail" placeholder="E-mail"/><br>
+            * <input type="text" name="title" placeholder="标题"/><br>
+            * <textarea name="desc" rows="8" placeholder="描述"/></textarea><br>
+            <!--* <span>请输入您的验证码</span> <input id="verify_code" type="text" name="verify_code"><br>-->
+            <input type="image" alt="提交" value="submit"/><br>
+        </form>
+    </div>
+    <div id="map">
+        Google Map
+    </div>
+</div>
 <div class="section" id="department_1">
     <div class="logo_banner">
         <a href="department.html?dep=0" id="logo_0" class="logo">
